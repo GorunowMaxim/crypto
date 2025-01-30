3d7712bafe362c564a132a750f967d18eb782d38
Gorunow Maxim <mgorunow@gmail.com>
2025-01-30 23:35:41 +0400

    change config

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { MainLayout } from "../layouts/mainLayout/ui/mainLayout";
+import { MainLayout } from "../layouts/mainLayout";
 
 import { RegisterPage } from "@/pages/registerPage";
 import { ErrorPage } from "@/pages/errorPage";
 import { HomePage } from "@/pages/home";
+import { DashboardLayout } from "../layouts/dashboardLayout";
 
 const Router = createBrowserRouter(
   [
@@ -19,10 +20,7 @@ const Router = createBrowserRouter(
         },
         {
           path: "/dashboard",
-          lazy: async () => {
-            let { DashboardLayout } = await import("../layouts/dashboardLayout");
-            return { Component: DashboardLayout };
-          },
+          element: <DashboardLayout />,
           children: [
             {
               index: true,
