2e8180b808dc95edf1a4e03904dca1811d588de9
Gorunow Maxim <mgorunow@gmail.com>
2025-01-31 00:16:11 +0400

    fix

--- src/pages/registerPage/ui/registerPage.tsx
+++ src/pages/registerPage/ui/registerPage.tsx
@@ -1,7 +1,10 @@
+import { NavLink } from "react-router-dom"
+
 export const RegisterPage = () => {
   return(
     <div className="w-full h-full flex items-center justify-center">
       <form className="w-[380px] h-[475px] bg-white"></form>
+      <NavLink to={'/dashboard'}>go to dashboard</NavLink>
     </div>
   )
 }
